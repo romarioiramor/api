dc9b596376e3b9ff3c43fe9dbcd3333d24e76167
romario <romario.adsistemas@gmail.com>
2021-03-21 15:44:07 -0300

    adicionando dados na api

--- database/database.js
+++ database/database.js
@@ -5,25 +5,64 @@ module.exports = {
     ],
     cidades:[
         'sp',
-        'rj'
+        'rj',
+        'df'
     ],
     dados:{
         sp:[{
-            data: '19/03/2021',
+            data: '21/03/2021',
             casosConfirmados: 1000,
             casosDia: 300,
             obitosDia: 200,
             obitosTotal: 2000
+        },{
+            data: '20/03/2021',
+            casosConfirmados: 700,
+            casosDia: 50,
+            obitosDia: 100,
+            obitosTotal: 900
+        },{
+            data: '18/03/2021',
+            casosConfirmados: 600,
+            casosDia: 100,
+            obitosDia: 80,
+            obitosTotal: 880
         },
         {
+            data: '17/03/2021',
+            casosConfirmados: 1100,
+            casosDia: 350,
+            obitosDia: 150,
+            obitosTotal: 2000
+        }],
+        df:[{
+            data: '21/03/2021',
+            casosConfirmados: 1000,
+            casosDia: 300,
+            obitosDia: 200,
+            obitosTotal: 2000
+        },{
             data: '20/03/2021',
+            casosConfirmados: 700,
+            casosDia: 50,
+            obitosDia: 100,
+            obitosTotal: 900
+        },{
+            data: '19/03/2021',
+            casosConfirmados: 600,
+            casosDia: 100,
+            obitosDia: 80,
+            obitosTotal: 880
+        },
+        {
+            data: '18/03/2021',
             casosConfirmados: 1100,
             casosDia: 350,
             obitosDia: 150,
             obitosTotal: 2000
         }],
         rj:[{
-            data: '19/03/2021',
+            data: '21/03/2021',
             casosConfirmados: 1000,
             casosDia: 300,
             obitosDia: 200,
@@ -37,14 +76,14 @@ module.exports = {
             obitosTotal: 2000
         },
         {
-            data: '21/03/2021',
+            data: '19/03/2021',
             casosConfirmados: 900,
             casosDia: 150,
             obitosDia: 50,
             obitosTotal: 1000
         },
         {
-            data: '22/03/2021',
+            data: '18/03/2021',
             casosConfirmados: 800,
             casosDia: 50,
             obitosDia: 150,
